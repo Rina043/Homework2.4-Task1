417140219b0108094ea9d0f6be49e2354f424367
Ekaterina Shipkova <katerinashipkova@gmail.com>
2020-08-07 20:41:20 +0400

    Code Updated

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,12 +1,66 @@
 package ru.netology.stats;
 
 public class StatsService {
-    public long calculateSum (long[] sales){
-        long sum = 0;
+    public int calculateSum (int[] sales){
+        int sum = 0;
         for (long sale : sales){
            sum += sale;
         }
         return sum;
     }
+
+      public int calculateAverage (int[] sales){
+        int amountofSum = calculateSum(sales);
+        int months = sales.length;
+          int average = amountofSum / months;
+
+        return average;
 }
 
+public int findMax (int[] sales){
+int numberMaxMonth = 0;
+        int maxSale = sales[0];
+        for (int i =0;i < sales.length; i++){
+                if (maxSale<= sales[i]){
+                    maxSale = sales[i];
+                    numberMaxMonth = i + 1;
+                }
+     }
+return numberMaxMonth;
+ }
+
+ public int findMin (int[] sales){
+        int numberMinMonth = 0;
+        int minSale = sales [0];
+        for (int i = 0; i< sales.length; i++){
+            if (minSale>= sales[i]){
+                minSale = sales[i];
+                numberMinMonth = i + 1;
+            }
+} return numberMinMonth;
+ }
+ public int LessThanAverage (int [] sales){
+        int average = calculateAverage(sales);
+        int sum = 0;
+        for (int sale : sales){
+            if (sale < average){
+                sale=1;
+                sum += sale;
+            }
+
+        }
+     return sum;
+ }
+
+ public int MoreThanAverage (int [] sales){
+        int average = calculateAverage(sales);
+        int sum = 0;
+        for (int sale : sales){
+            if (sale > average){
+                sale=1;
+                sum += sale;
+            }
+        }
+        return sum;
+ }
+}
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -6,15 +6,60 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatsServiceTest {
-    long [] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+    int [] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
     @Test
     void shouldCalculateSum () {
 
         StatsService service = new StatsService();
 
-        long expected = 180;
-        long actual = service.calculateSum(sales);
+        int expected = 180;
+        int actual = service.calculateSum(sales);
         assertEquals(expected, actual);
     }
+
+@Test
+
+void shouldCalculateAverage () {
+    StatsService service = new StatsService ();
+
+    int expected = 15;
+    int actual = service.calculateAverage(sales);
+    assertEquals(expected, actual);
+}
+
+@Test
+    void shouldCalculateMax () {
+        StatsService service = new StatsService ();
+        int [] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+        int actual = service.findMax(sales);
+    assertEquals(expected, actual);
+}
+
+@Test
+    void shouldCalculateMin (){
+        StatsService service = new StatsService ();
+    int [] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+    int expected = 9;
+    int actual = service.findMin(sales);
+    assertEquals(expected, actual);
+}
+
+@Test
+    void shouldCalculateLessThanAverage(){
+        StatsService service = new StatsService ();
+    int [] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.LessThanAverage(sales);
+    assertEquals(expected, actual);
+}
+ @Test
+    void shouldCalculateMoreThanAverage(){
+        StatsService service = new StatsService ();
+     int [] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+     int expected = 5;
+     int actual = service.MoreThanAverage(sales);
+     assertEquals(expected, actual);
+ }
 }
